8ae6291a4631b3481814b464776ad2cb2d33546e
hutcb591 <brett.hutchinson22@icloud.com>
2026-05-17 13:20:31 -0600

    wrapper update

--- replay.pokemonshowdown.com/theme/wrapper.inc.template.php
+++ replay.pokemonshowdown.com/theme/wrapper.inc.template.php
@@ -26,12 +26,12 @@ function ThemeHeaderTemplate() {
 <?php } ?>
 
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=IE8" />
-	<link rel="stylesheet" href="//play.pokemonshowdown.com/style/font-awesome.css" />
-	<link rel="stylesheet" href="//pokemonshowdown.com/theme/panels.css" />
-	<link rel="stylesheet" href="//pokemonshowdown.com/theme/main.css" />
-	<link rel="stylesheet" href="//play.pokemonshowdown.com/style/battle.css?" />
-	<link rel="stylesheet" href="//play.pokemonshowdown.com/style/replay.css?" />
-	<link rel="stylesheet" href="//play.pokemonshowdown.com/style/utilichart.css?" />
+	<link rel="stylesheet" href="https://aslclientshowdown-production.up.railway.app/style/font-awesome.css" />
+	<link rel="stylesheet" href="https://aslclientshowdown-production.up.railway.app/theme/panels.css" />
+	<link rel="stylesheet" href="https://aslclientshowdown-production.up.railway.app/theme/main.css" />
+	<link rel="stylesheet" href="https://aslclientshowdown-production.up.railway.app/style/battle.css?" />
+	<link rel="stylesheet" href="https://aslclientshowdown-production.up.railway.app/style/replay.css?" />
+	<link rel="stylesheet" href="https://aslclientshowdown-production.up.railway.app/style/utilichart.css?" />
 
 	<!-- Workarounds for IE bugs to display trees correctly. -->
 	<!--[if lte IE 6]><style> li.tree { height: 1px; } </style><![endif]-->
@@ -76,10 +76,10 @@ function ThemeHeaderTemplate() {
 
 function ThemeScriptsTemplate() {
 ?>
-	<script src="//play.pokemonshowdown.com/js/lib/jquery-1.11.0.min.js"></script>
-	<script src="//play.pokemonshowdown.com/js/lib/lodash.core.js"></script>
-	<script src="//play.pokemonshowdown.com/js/lib/backbone.js"></script>
-	<script src="//dex.pokemonshowdown.com/js/panels.js"></script>
+	<script src="https://aslclientshowdown-production.up.railway.app/js/lib/jquery-1.11.0.min.js"></script>
+	<script src="https://aslclientshowdown-production.up.railway.app/js/lib/lodash.core.js"></script>
+	<script src="https://aslclientshowdown-production.up.railway.app/js/lib/backbone.js"></script>
+	<script src="https://aslclientshowdown-production.up.railway.app/js/panels.js"></script>
 <?php
 }
 
@@ -88,22 +88,22 @@ function ThemeFooterTemplate() {
 ?>
 <?php $panels->scripts(); ?>
 
-	<script src="//play.pokemonshowdown.com/js/lib/jquery-cookie.js"></script>
-	<script src="//play.pokemonshowdown.com/js/lib/html-sanitizer-minified.js"></script>
-	<script src="//play.pokemonshowdown.com/js/battle-sound.js?"></script>
-	<script src="//play.pokemonshowdown.com/config/config.js?"></script>
-	<script src="//play.pokemonshowdown.com/js/battledata.js?"></script>
-	<script src="//play.pokemonshowdown.com/data/pokedex-mini.js?"></script>
-	<script src="//play.pokemonshowdown.com/data/pokedex-mini-bw.js?"></script>
-	<script src="//play.pokemonshowdown.com/data/graphics.js?"></script>
-	<script src="//play.pokemonshowdown.com/data/pokedex.js?"></script>
-	<script src="//play.pokemonshowdown.com/data/items.js?"></script>
-	<script src="//play.pokemonshowdown.com/data/moves.js?"></script>
-	<script src="//play.pokemonshowdown.com/data/abilities.js?"></script>
-	<script src="//play.pokemonshowdown.com/data/teambuilder-tables.js?"></script>
-	<script src="//play.pokemonshowdown.com/js/battle-tooltips.js?"></script>
-	<script src="//play.pokemonshowdown.com/js/battle.js?"></script>
-	<script src="/js/replay.js?"></script>
+	<script src="https://aslclientshowdown-production.up.railway.app/js/lib/jquery-cookie.js"></script>
+	<script src="https://aslclientshowdown-production.up.railway.app/js/lib/html-sanitizer-minified.js"></script>
+	<script src="https://aslclientshowdown-production.up.railway.app/js/battle-sound.js?"></script>
+	<script src="https://aslclientshowdown-production.up.railway.app/config/config.js?"></script>
+	<script src="https://aslclientshowdown-production.up.railway.app/js/battledata.js?"></script>
+	<script src="https://aslclientshowdown-production.up.railway.app/data/pokedex-mini.js?"></script>
+	<script src="https://aslclientshowdown-production.up.railway.app/data/pokedex-mini-bw.js?"></script>
+	<script src="https://aslclientshowdown-production.up.railway.app/data/graphics.js?"></script>
+	<script src="https://aslclientshowdown-production.up.railway.app/data/pokedex.js?"></script>
+	<script src="https://aslclientshowdown-production.up.railway.app/data/items.js?"></script>
+	<script src="https://aslclientshowdown-production.up.railway.app/data/moves.js?"></script>
+	<script src="https://aslclientshowdown-production.up.railway.app/data/abilities.js?"></script>
+	<script src="https://aslclientshowdown-production.up.railway.app/data/teambuilder-tables.js?"></script>
+	<script src="https://aslclientshowdown-production.up.railway.app/js/battle-tooltips.js?"></script>
+	<script src="https://aslclientshowdown-production.up.railway.app/js/battle.js?"></script>
+	<script src="https://aslclientshowdown-production.up.railway.app/js/replay.js?"></script>
 
 </body></html>
 <?php
