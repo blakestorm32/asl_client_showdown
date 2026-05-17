5ee264fca0ea39467caa93ffc95a5111e0788695
hutcb591 <brett.hutchinson22@icloud.com>
2026-05-17 11:16:19 -0600

    dmg calc

--- pokemonshowdown.com/news/include.php
+++ pokemonshowdown.com/news/include.php
@@ -13,15 +13,39 @@ function renderNews() {
 	global $latestNewsCache, $newsCache;
 	$buf = '';
 	$count = 0;
+
 	foreach ($latestNewsCache as $topic_id) {
 		$topic = $newsCache[$topic_id];
+
 		$buf .= '<div class="newsentry" data-newsid="'.$topic_id.'" data-date="'.$topic['date'].'">';
 		$buf .= '<h4>'.$topic['title_html'].'</h4>';
 		$buf .= @$topic['summary_html'];
-		$buf .= '<p>&mdash;<strong>'.$topic['authorname'].'</strong> <small class="date">on '.readableDate($topic['date']).'</small>'.(isset($topic['details']) ? ' <small><a href="http://pokemonshowdown.com/news/'.$topic['topic_id'].'" target="_blank">Read more</a></small>' : '').'</p>';
+
+		$buf .= '<p>&mdash;<strong>'.$topic['authorname'].'</strong> ';
+		$buf .= '<small class="date">on '.readableDate($topic['date']).'</small>';
+
+		if (isset($topic['details'])) {
+			$buf .= ' <small><a href="http://pokemonshowdown.com/news/'.$topic['topic_id'].'" target="_blank">Read more</a></small>';
+		}
+
+		$buf .= '</p>';
 		$buf .= '</div>';
+
 		if (++$count >= 2) break;
 	}
+
+	// Custom calculator news box
+	$buf .= '<div class="newsentry broadcast-blue" data-newsid="9999">';
+	$buf .= '<h4>Damage Calculators</h4>';
+
+	$buf .= '<p><a href="https://aslchampions-damage-calc.up.railway.app/" target="_blank">';
+	$buf .= 'Champions/Mystic Damage Calculator</a></p>';
+
+	$buf .= '<p><a href="https://aslrelicanth-damage-calc.up.railway.app/" target="_blank">';
+	$buf .= 'ASL Relicanth Calculator</a></p>';
+
+	$buf .= '</div>';
+
 	return $buf;
 }
 
